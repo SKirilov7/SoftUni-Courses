cc5aac03f119ea064939380907b59dd87de1a9ed
SKirilov7 <88241989+SKirilov7@users.noreply.github.com>
2021-09-25 16:24:35 +0300

    Updating one task from multidimensional execise.

--- PythonADVANCED/Multidimensional_lists/Exercise/06.MatrixShuffling.py
+++ PythonADVANCED/Multidimensional_lists/Exercise/06.MatrixShuffling.py
@@ -1,8 +1,5 @@
-def index_validator(matr,row_1,col_1,row_2,col_2):
-    if 0 <= row_1 < len(matr) and 0 <= row_2 < len(matr):
-        if 0 <= col_1 < len(matr[row_1]) and 0 <= col_2 < len(matr[row_2]):
-            return True
-    return False
+def index_validator(rows,cols,row_1,col_1,row_2,col_2):
+    return 0 <= row_1 < rows and 0 <= row_2 < rows and 0 <= col_1 < cols and 0 <= col_2 < cols
 
 
 rows,cols = [int(num) for num in input().split()]
@@ -13,7 +10,7 @@ while not command == 'END':
     try:
         current_command ,*numbers = command.split()
         row_one,col_one,row_two,col_two = [int(num) for num in numbers]
-        if not current_command == 'swap' or not index_validator(matrix,row_one,col_one,row_two,col_two):
+        if not current_command == 'swap' or not index_validator(rows,cols,row_one,col_one,row_two,col_two):
             print('Invalid input!')
             command = input()
             continue
