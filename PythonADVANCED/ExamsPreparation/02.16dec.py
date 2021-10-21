1c58e37b584e472ee7cf957a77dc70185463328a
SKirilov7 <88241989+SKirilov7@users.noreply.github.com>
2021-10-21 20:10:50 +0300

    Updating tasks from exam preparation.

--- PythonADVANCED/ExamsPreparation/02.16dec.py
+++ PythonADVANCED/ExamsPreparation/02.16dec.py
@@ -1,46 +1,41 @@
 def create_matrix_and_find_player(row_col):
-    matrix, pl_row, pl_col = [], None, None
+    matrx, p_row, p_col = [], 0, 0
     for row in range(row_col):
         current_col = list(input())
+        matrx.append(current_col)
         if 'P' in current_col:
-            pl_row,pl_col = row, current_col.index('P')
-        matrix.append(current_col)
-    return matrix, pl_row, pl_col
+            p_row, p_col = row, current_col.index('P')
+    return matrx, p_row, p_col
 
 
-def is_valid_index(row_col, r, c):
+def is_valid_indexes(r, c, row_col):
     return 0 <= r < row_col and 0 <= c < row_col
 
 
-operation_mapper = {
-    'left': [0, -1],
-    'right': [0, 1],
-    'up': [-1, 0],
-    'down': [1, 0]
+direction_mapper = {
+    'right': (0, 1),
+    'left': (0, -1),
+    'up': (-1, 0),
+    'down': (1, 0)
 }
 
-string = input()
+initial_string = input()
 rows_cols = int(input())
 matrix, player_row, player_col = create_matrix_and_find_player(rows_cols)
-
-
 number_commands = int(input())
 
 for _ in range(number_commands):
     command = input()
-    wanted_movement_row = player_row + operation_mapper[command][0]
-    wanted_movement_col = player_col + operation_mapper[command][1]
-
-    if not is_valid_index(rows_cols, wanted_movement_row, wanted_movement_col) and string:
-        string = string[0:-1]
+    next_row, next_col = player_row + direction_mapper[command][0], player_col + direction_mapper[command][1]
+    if not is_valid_indexes(next_row, next_col, rows_cols) and initial_string:
+        initial_string = initial_string[:-1]
         continue
 
-    elif is_valid_index(rows_cols, wanted_movement_row, wanted_movement_col) and matrix[wanted_movement_row][wanted_movement_col].isalpha():
-        string += matrix[wanted_movement_row][wanted_movement_col]
+    if matrix[next_row][next_col].isalpha():
+        initial_string += matrix[next_row][next_col]
 
-    matrix[player_row][player_col] = '-'
-    matrix[wanted_movement_row][wanted_movement_col] = 'P'
-    player_row, player_col = wanted_movement_row, wanted_movement_col
+    matrix[player_row][player_col], matrix[next_row][next_col] = '-', 'P'
+    player_row, player_col = next_row, next_col
 
-print(string)
+print(initial_string)
 [print(*row, sep='') for row in matrix]
