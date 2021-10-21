ac6458c36463d4601f5c04fa2313a540d3e6d801
SKirilov7 <88241989+SKirilov7@users.noreply.github.com>
2021-10-21 19:29:05 +0300

    Updating two tasks from exam preparations.

--- PythonADVANCED/ExamsPreparation/02.14feb.py
+++ PythonADVANCED/ExamsPreparation/02.14feb.py
@@ -1,55 +1,50 @@
 def create_matrix_and_find_player(row_col):
-    matrix = []
-    plr_row, plr_col = 0, 0
+    matrx, p_row, p_col = [], 0, 0
     for row in range(row_col):
         current_col = input().split()
-        matrix.append(current_col)
+        matrx.append(current_col)
         if 'P' in current_col:
-            plr_row, plr_col = row, current_col.index('P')
-    return matrix, plr_row, plr_col
+            p_row, p_col = row, current_col.index('P')
+    return matrx, p_row, p_col
 
 
-def is_valid_move(row_col, r, c):
+def is_valid_indexes(r, c, row_col):
     return 0 <= r < row_col and 0 <= c < row_col
 
 
-operation_mapper = {
-    'up': [-1, 0],
-    'down': [1, 0],
-    'left': [0, -1],
-    'right': [0, 1]
+movement_operator = {
+    'right': (0, 1),
+    'left': (0, -1),
+    'up': (-1, 0),
+    'down': (1, 0)
 }
 
 rows_cols = int(input())
-field, player_row, player_col = create_matrix_and_find_player(rows_cols)
+matrix, player_row, player_col = create_matrix_and_find_player(rows_cols)
 
-coins_obtained = 0
-path_of_player = []
+coins_collected = 0
+movements = []
+
 while True:
     command = input()
-    if command not in operation_mapper:
+    if not command in movement_operator:
         continue
+    next_row, next_col = player_row + movement_operator[command][0], player_col + movement_operator[command][1]
 
-    wanted_move_row, wanted_move_col = player_row + operation_mapper[command][0], player_col + operation_mapper[command][1]
-    if not is_valid_move(rows_cols, wanted_move_row, wanted_move_col) or field[wanted_move_row][wanted_move_col] == 'X':
-        coins_obtained = coins_obtained // 2
+    if not is_valid_indexes(next_row, next_col, rows_cols) or matrix[next_row][next_col] == 'X':
+        coins_collected //= 2
         break
 
-    coins_obtained += int(field[wanted_move_row][wanted_move_col])
-    path_of_player.append([wanted_move_row, wanted_move_col])
-    if coins_obtained >= 100:
+    coins_collected += int(matrix[next_row][next_col])
+    movements.append([next_row, next_col])
+    matrix[player_row][player_col], matrix[next_row][next_col] = 0, 'P'
+    player_row, player_col = next_row, next_col
+
+    if coins_collected >= 100:
         break
 
-    player_row, player_col = wanted_move_row, wanted_move_col
 
-if coins_obtained >= 100:
-    print(f"You won! You've collected {coins_obtained} coins.")
-else:
-    print(f"Game over! You've collected {coins_obtained} coins.")
-
-print(f"Your path: ")
-[print(row) for row in path_of_player]
-
-
-
-
+print(f"You won! You've collected {coins_collected} coins.") if coins_collected >= 100 \
+    else print(f"Game over! You've collected {coins_collected} coins.")
+print('Your path:')
+[print(indexes) for indexes in movements]
--- PythonADVANCED/ExamsPreparation/03.14feb.py
+++ PythonADVANCED/ExamsPreparation/03.14feb.py
@@ -1,27 +1,11 @@
 def stock_availability(boxes, command, *args):
     if command == 'delivery':
-        [boxes.append(arg) for arg in args]
-        return boxes
-
+        return boxes + list(args)
     elif command == 'sell' and not args:
         return boxes[1:]
-
-    for arg in args:
-        try:
-            boxes = boxes[int(arg):]
-        except ValueError:
-            while arg in boxes:
-                boxes.remove(arg)
+    elif command == 'sell' and isinstance(args[0], int):
+        return boxes[args[0]:]
+    for box in set(args):
+        while box in boxes:
+            boxes.remove(box)
     return boxes
-
-
-
-
-print(stock_availability(["choco", "vanilla", "banana"], "delivery", "caramel", "berry"))
-print(stock_availability(["chocolate", "vanilla", "banana"], "delivery", "cookie","banana"))
-print(stock_availability(["chocolate", "vanilla", "banana"], "sell"))
-print(stock_availability(["chocolate", "vanilla", "banana"], "sell", 3))
-print(stock_availability(["chocolate", "chocolate", "banana"], "sell", "chocolate"))
-print(stock_availability(["cookie", "chocolate", "banana"], "sell", "chocolate"))
-print(stock_availability(["chocolate", "vanilla", "banana"], "sell", "cookie"))
-
