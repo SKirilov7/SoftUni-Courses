0743158866d1505569d1a4e67c0cfbae66ca4f7c
SKirilov7 <88241989+SKirilov7@users.noreply.github.com>
2021-10-22 19:48:16 +0300

    Updating two tasks from exam prep.

--- PythonADVANCED/ExamsPreparation/02.Snake.py
+++ PythonADVANCED/ExamsPreparation/02.Snake.py
@@ -1,82 +1,47 @@
-def create_matrix_and_find_snake(row_col):
+def create_matrix_and_find_snake_and_burrows(row_col):
     matrix, s_row, s_col = [], 0, 0
-    first_burrow = []
-    second_burrow = []
     for row in range(row_col):
         current_col = list(input())
         matrix.append(current_col)
         if 'S' in current_col:
             s_row, s_col = row, current_col.index('S')
-        if 'B' in current_col:
-            if not first_burrow:
-                first_burrow = [row, current_col.index('B')]
-            elif not second_burrow:
-                second_burrow = [row, current_col.index('B')]
-    return matrix, s_row, s_col, first_burrow, second_burrow
+    return matrix, s_row, s_col,
 
 
-def movement_operator(r,c,command_data):
-    if command_data == 'left':
-        c -= 1
-    elif command_data == 'right':
-        c += 1
-    elif command_data == 'up':
-        r -= 1
-    elif command_data == 'down':
-        r += 1
-    return r, c
-
-
-def is_valid_index(row_col, r, c):
+def is_valid_indexes(r, c, row_col):
     return 0 <= r < row_col and 0 <= c < row_col
 
 
+movement_operator = {
+    'left': (0, -1),
+    'right': (0, 1),
+    'up': (-1, 0),
+    'down': (1, 0)
+}
+
 rows_cols = int(input())
-field, snake_row, snake_col, first_burrow, second_burrow = create_matrix_and_find_snake(rows_cols)
+matrix, snake_row, snake_col = create_matrix_and_find_snake_and_burrows(rows_cols)
 
 food_eaten = 0
 while True:
     command = input()
-    next_row, next_col = movement_operator(snake_row, snake_col, command)
-    if not is_valid_index(rows_cols, next_row, next_col):
-        field[snake_row][snake_col] = '.'
-        print('Game over!')
+    next_row, next_col = snake_row + movement_operator[command][0], snake_col + movement_operator[command][1]
+    matrix[snake_row][snake_col] = '.'
+    if not is_valid_indexes(next_row, next_col, rows_cols):
         break
 
-    if field[next_row][next_col] == 'B' and (first_burrow or second_burrow):
-        field[snake_row][snake_col] = '.'
+    if matrix[next_row][next_col] == 'B':
+        matrix[next_row][next_col] = '.'
+        next_row, next_col = [[row, col] for row in range(len(matrix)) for col in range(len(matrix[row])) if matrix[row][col] == 'B'][0]
+    elif matrix[next_row][next_col] == '*':
+        food_eaten += 1
 
-    if field[next_row][next_col] == 'B' and [next_row, next_col] == first_burrow:
-        field[next_row][next_col] = '.'
-        snake_row, snake_col = second_burrow[0], second_burrow[1]
-    elif field[next_row][next_col] == 'B' and [next_row, next_col] == second_burrow:
-        field[next_row][next_col] = '.'
-        snake_row, snake_col = first_burrow[0], first_burrow[1]
+    matrix[next_row][next_col] = 'S'
+    snake_row, snake_col = next_row, next_col
 
-    else:
-        if field[next_row][next_col] == '*':
-            food_eaten += 1
-        field[snake_row][snake_col] = '.'
-        snake_row, snake_col = next_row, next_col
-
-    field[snake_row][snake_col] = 'S'
     if food_eaten >= 10:
-        print(f"You won! You fed the snake.")
         break
 
-
+print(f"You won! You fed the snake.") if food_eaten >= 10 else print('Game over!')
 print(f"Food eaten: {food_eaten}")
-[print(*row,sep='') for row in field]
-
-
-
-
-
-
-
-
-
-
-
-
-
+[print(*row, sep='') for row in matrix]
--- PythonADVANCED/ExamsPreparation/03.ListManipulator.py
+++ PythonADVANCED/ExamsPreparation/03.ListManipulator.py
@@ -1,18 +1,11 @@
-def list_manipulator(numbers, command, beginning_or_end, *args):
+def list_manipulator(numbers, command,beginning_or_end, *args):
     if command == 'add' and beginning_or_end == 'beginning':
-        numbers = list(args) + numbers
-    elif command == 'add' and beginning_or_end == 'end':
-        [numbers.append(num) for num in args]
-    elif command == 'remove' and beginning_or_end == 'beginning' and args:
-        numbers = numbers[0 + args[0]:]
-    elif command == 'remove' and beginning_or_end == 'beginning' and not args:
-        numbers = numbers[1:]
-    elif command == 'remove' and beginning_or_end == 'end' and args:
-        numbers = numbers[:len(numbers) - args[0]]
-    elif command == 'remove' and beginning_or_end == 'end' and not args:
-        numbers.pop()
-
-    return numbers
+        return list(args) + numbers
+    if command == 'add' and beginning_or_end == 'end':
+        return numbers + list(args)
+    if command == 'remove' and beginning_or_end == 'beginning':
+        return numbers[args[0]:] if args else numbers[1:]
+    return numbers[:-args[0]] if args else numbers[:-1]
 
 
 print(list_manipulator([1,2,3], "remove", "end"))
