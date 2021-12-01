918434fd9db262fc2ce92c4f8dcacc65fa4b2cfc
SKirilov7 <88241989+SKirilov7@users.noreply.github.com>
2021-12-01 20:33:10 +0200

    Changing file in tests for Exam22.08.2020

--- PythonOOP/ExamPreparation/Exam22.08.2020/Tests/test/test.py
+++ PythonOOP/ExamPreparation/Exam22.08.2020/Tests/test/test.py
@@ -1,73 +1,69 @@
-from Tests.student_report_card import StudentReportCard
+from project.student_report_card import StudentReportCard
 from unittest import TestCase, main
 
 
 class TestStudentReportCard(TestCase):
-    def setUp(self):
-        self.student_card = StudentReportCard('student', 12)
+    def setUp(self) -> None:
+        self.student_card = StudentReportCard('Pesho', 12)
 
     def test_init_attaches_correctly(self):
-        self.assertEqual('student', self.student_card.student_name)
+        self.assertEqual('Pesho', self.student_card.student_name)
         self.assertEqual(12, self.student_card.school_year)
         self.assertEqual({}, self.student_card.grades_by_subject)
 
-    def test_if_empty_value_for_name_raises(self):
+    def test_student_name_empty_string_raises(self):
         with self.assertRaises(ValueError) as ex:
-            self.student_card.student_name = ''
+            student = StudentReportCard('', 10)
         self.assertEqual("Student Name cannot be an empty string!", str(ex.exception))
+        self.student_card.student_name = 'Slavi'
+        self.assertEqual('Slavi', self.student_card.student_name)
 
-    def test_school_year_must_be_between_1_and_12(self):
+    def test_school_year_less_than_1_and_more_than_12_raises(self):
         with self.assertRaises(ValueError) as ex:
-            self.student_card.school_year = 0
+            student = StudentReportCard('Kiro', 0)
         self.assertEqual("School Year must be between 1 and 12!", str(ex.exception))
+        with self.assertRaises(ValueError) as exception_error:
+            student = StudentReportCard('Kiro', 13)
+        self.assertEqual("School Year must be between 1 and 12!", str(exception_error.exception))
+        self.student_card.school_year = 1
+        self.assertEqual(1, self.student_card.school_year)
 
-        with self.assertRaises(ValueError) as ex:
-            self.student_card.school_year = 13
-        self.assertEqual("School Year must be between 1 and 12!", str(ex.exception))
-
-    def test_add_grade_with_non_existing_subject(self):
+    def test_add_grade_with_non_existing_and_existing_subject(self):
         self.assertEqual({}, self.student_card.grades_by_subject)
-        self.student_card.add_grade('Math', 5.2)
-        self.assertEqual({'Math': [5.2]}, self.student_card.grades_by_subject)
-
-    def test_add_grade_with_existing_subject(self):
-        self.student_card.grades_by_subject = {'Math': [5.2]}
+        self.student_card.add_grade('Math', 5)
+        self.assertEqual({'Math': [5]}, self.student_card.grades_by_subject)
         self.student_card.add_grade('Math', 6)
-        self.assertEqual({'Math': [5.2, 6]}, self.student_card.grades_by_subject)
+        self.assertEqual({'Math': [5, 6]}, self.student_card.grades_by_subject)
+        self.student_card.add_grade('Physics', 6)
+        self.assertEqual({'Math': [5, 6], 'Physics': [6]}, self.student_card.grades_by_subject)
 
-    def test_average_grade_with_non_existing_subjects(self):
+    def test_average_grade_by_subject_method(self):
         self.assertEqual({}, self.student_card.grades_by_subject)
-        return_message = self.student_card.average_grade_by_subject()
-        self.assertEqual('', return_message)
-
-    def test_average_grade_with_existing_subjects(self):
-        self.student_card.grades_by_subject = {'Math': [5, 6], 'Art': [6, 5]}
-        expected = 'Math: 5.50\nArt: 5.50'
-        return_message = self.student_card.average_grade_by_subject()
-        self.assertEqual(expected, return_message)
+        actual = self.student_card.average_grade_by_subject()
+        self.assertEqual(actual, '')
+        self.student_card.grades_by_subject = {'Math': [5, 6], 'Physics': [5, 6]}
+        expected = 'Math: 5.50\nPhysics: 5.50'
+        actual = self.student_card.average_grade_by_subject()
+        self.assertEqual(actual, expected)
 
     def test_average_grade_for_all_subjects(self):
-        self.student_card.grades_by_subject = {'Math': [5, 6], 'Art': [6, 5]}
+        self.student_card.grades_by_subject = {'Math': [5, 5], 'Physics': [6, 6]}
         expected = 'Average Grade: 5.50'
-        return_message = self.student_card.average_grade_for_all_subjects()
-        self.assertEqual(expected, return_message)
-
-    def test_average_grade_for_all_subjects_with_no_subjects(self):
-        with self.assertRaises(ZeroDivisionError) as ex:
-            self.student_card.average_grade_for_all_subjects()
-        self.assertEqual('division by zero', str(ex.exception))
+        actual = self.student_card.average_grade_for_all_subjects()
+        self.assertEqual(actual, expected)
 
     def test_represent_method(self):
-        self.student_card.grades_by_subject = {'Math': [5, 6], 'Art': [6, 5]}
-        expected = f"Name: student\n" \
-                   f"Year: 12\n" \
-                   f"----------\n" \
-                   f"Math: 5.50\n" \
-                   f"Art: 5.50\n" \
-                   f"----------\n" \
-                   f"Average Grade: 5.50"
-        return_message = repr(self.student_card)
-        self.assertEqual(expected, return_message)
+        self.student_card.grades_by_subject = {'Math': [5, 6], 'Physics': [5, 6]}
+        self.assertEqual({'Math': [5, 6], 'Physics': [5, 6]}, self.student_card.grades_by_subject)
+        expected = 'Name: Pesho\n' \
+                   'Year: 12\n' \
+                   '----------\n' \
+                   'Math: 5.50\n' \
+                   'Physics: 5.50\n' \
+                   '----------\n' \
+                   'Average Grade: 5.50'
+        actual = self.student_card.__repr__()
+        self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
